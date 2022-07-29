5c16a8de767b7126e62464f95876786c1dd52e1a
Max <max@privatevoid.net>
2022-07-29 11:01:24 +0200

    bubblewrap: support binary bind

--- modules/bubblewrap.nix
+++ modules/bubblewrap.nix
@@ -11,25 +11,37 @@ let
     name = "path specifier";
     check = x: strings.isCoercibleToString x && builtins.elem (builtins.substring 0 1 (toString x)) [ "/" "$" ];
   };
+
+  pairOf = elemType: with types; let
+    list = nonEmptyListOf elemType;
+    checked = addCheck list (l: length l == 2);
+  in checked // {
+    description = "pair of ${elemType.description}";
+  };
+
+  bindType = with types; listOf (oneOf [
+    (pairOf envPathType)
+    envPathType
+  ]);
 in {
   options.bubblewrap = {
     network = mkEnableOption "network access in the sandbox" // { default = true; };
 
     bind.rw = mkOption {
       description = "Read-write paths to bind-mount into the sandbox.";
-      type = with types; listOf envPathType;
+      type = bindType;
       default = [];
     };
 
     bind.ro = mkOption {
       description = "Read-only paths to bind-mount into the sandbox.";
-      type = with types; listOf envPathType;
+      type = bindType;
       default = [];
     };
 
     bind.dev = mkOption {
       description = "Devices to bind-mount into the sandbox.";
-      type = with types; listOf envPathType;
+      type = bindType;
       default = [];
     };
 
